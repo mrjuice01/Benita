287033212f47c90834568bfc04a7d2c08d8a2220
Mr Juice <chigonyetawanda@gmail.com>
2023-09-07 12:04:56 +0200

    Diego💩

---
+++ config.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+global.available = true;
+global.autoReadGc = true;
+global.autoReadAll = false;
+global.antitags = true;
+
+global.Owner = ["263780699988"];
+global.OwnerNumber = ["263780699988"];
+global.ownertag = ["263780699988"];
+global.BotName = "BayMax";
+global.OwnerName = "Mr Juice";
+global.sessionName = "session";
+global.openAiAPI = "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" // Put openapi key here
+
+//   Prefix  //
+global.prefa = ["."];
+
+global.limitawal = {
+  premium: "Infinity",
+  free: 2,
+  monayawal: 1000,
+};
+
+global.limitawal = {
+  rakyat: "Infinity",
+  free: 100,
+};
+
+global.APIs = {
+  zenz: "https://zenzapis.xyz",
+};
+global.APIKeys = {
+  "https://zenzapis.xyz": "5d1197db351b",
+};
